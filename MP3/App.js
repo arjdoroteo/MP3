c0036ab1d616c0bb7b3e0e9d7e12966fcbafc7ec
Robert56th <adrian56th@gmail.com>
2022-02-09 15:48:26 -0800

    2nd commit with first screen

--- MP3/App.js
+++ MP3/App.js
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
+import Checkbox from 'expo-checkbox';
 
 export default function App() {
+  const [isYesChecked, setYesChecked] = useState(false);
+  const [isNoChecked, setNoChecked] = useState(false);
+  const createTwoButtonAlert = () =>
+    Alert.alert('Notice', 'You have to wait more than 48 hours of any of your dose to proceed', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+  function onYesCheck(){
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.rootContainer}>
+      <View style = {styles.upperContainer}>
+        <Image source={require('./assets/logo.png')}/>
+      </View>
+      <View style = {styles.lowerContainer}>
+        <Text style = {styles.textStyle}>Were you vaccinated more than 48 hours ago for any of your doses?</Text>
+        <View style = {styles.checkboxContainer}>
+          <Checkbox
+            style={styles.checkbox}
+            value={isYesChecked}
+            onValueChange={() => setYesChecked(!isYesChecked)}
+            color={isYesChecked ? '#4630EB' : undefined}
+          />
+          <Text style ={{margin:10, fontSize: 18}}>Yes</Text>
+        </View>
+        <View style = {styles.checkboxContainer}>
+          <Checkbox
+            style={styles.checkbox}
+            value={isNoChecked}
+            onValueChange={() => setNoChecked(createTwoButtonAlert)}
+            disabled = {isYesChecked}
+            color={isNoChecked ? '#4630EB' : undefined}
+          />
+          <Text style ={{margin:10, fontSize: 18}}>No</Text>
+        </View>
+        <View style = {styles.submitbtn}>
+          <Button
+          title = "Submit"
+          color = 'pink'
+          disabled = {!isYesChecked}
+          onPress = {null}>
+          </Button>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  upperContainer: {
+    backgroundColor:'white',
+    flex:1,
+    justifyContent: 'center'
+
+  },
+  lowerContainer: {
+    backgroundColor:'white',
+    flex:2,
+    margin: 30,
+  },
+  textStyle:{
+    fontSize:18
+  },
+  checkboxContainer:{
+    marginVertical: 5,
+    alignItems:'center',
+    flexDirection: 'row',
+    padding: 5
+  },
+  submitbtn:{
+    alignItems:'center',
+  }
 });
